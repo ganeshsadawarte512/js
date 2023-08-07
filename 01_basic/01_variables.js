a7a4e8dd4acbfb593245c099376278bcae095453
Ganesh sadawarte <129984079+ganeshsadawarte512@users.noreply.github.com>
2023-08-07 15:05:01 +0000

    datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "ganesh@gmail.com"
+var accountPassword ="1234"
+accountCity = "jaipur" 
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
